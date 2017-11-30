f6e6ba3eb8195c1f6b8b237f92f93dae79986725
bennybop <bennybop74@yahoo.com>
2017-11-30 02:22:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,22 +1,24 @@
 def sort_array_asc(array)
-  return array.sort
+   array.sort
 end
 
 def sort_array_desc(array)
-  return array.sort{|a, b| b <=> a}
+   array.sort{|a, b| b <=> a}
 end
 
 def sort_array_char_count(array)
-  return array.sort { |a, b| a.length <=> b.length}
+   array.sort { |a, b| a.length <=> b.length}
 end
 
 def swap_elements(array)
   element = array[1]
   array[1] = array[2]
   array[2] = element
-  return array
+   array
 end
 
 def reverse_array(array)
-  return array.reverse
+   array.reverse
 end
+def kesha_maker(array)
+  
